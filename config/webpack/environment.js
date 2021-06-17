421f8535750e2534b6dfba930726b362e9b1e075
Shoko <52348865+key0123456789@users.noreply.github.com>
2021-06-18 02:26:01 +0900

    initial commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,19 @@
+const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
+)
+
+environment.loaders.prepend('vue', {
+    test: /\.vue$/,
+    use: [{
+        loader: 'vue-loader'
+    }]
+})
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+module.exports = environment
