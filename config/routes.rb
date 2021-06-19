02987a6171c024d4309bc4f3cb7854d569b6d3b9
Shoko <52348865+key0123456789@users.noreply.github.com>
2021-06-20 02:40:36 +0900

    letter_opener

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resource :home, only: [:index]
 
   root to: 'home#index'
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
