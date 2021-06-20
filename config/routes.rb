ef7d68e27ab7039655a8516565b45f6600cc6ea0
Shoko <52348865+key0123456789@users.noreply.github.com>
2021-06-21 00:17:48 +0900

    login & logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, skip: [:sessions]
 
-  devise_for :users
-  resource :home, only: [:index]
+  devise_scope :user do
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    get 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   root to: 'home#index'
 
