acaafa2a3e02d3f7613d08b10ef7dba8de0dea13
Shoko <52348865+key0123456789@users.noreply.github.com>
2021-06-29 01:55:42 +0900

    set time zone

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,7 @@ module ShabonDog
   class Application < Rails::Application
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.time_zone_aware_types = [:datetime, :time]
   end
 end
